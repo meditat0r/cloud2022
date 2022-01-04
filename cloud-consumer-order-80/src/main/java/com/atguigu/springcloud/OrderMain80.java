8e0d9dfd205aa54728d88fb9b8124105dacba2b6
meditat0r <2523398873@qq.com>
2022-01-04 10:37:35 +0800

    相关启动类添加exclude={DataSourceAutoConfiguration.class}

--- cloud-consumer-order-80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ cloud-consumer-order-80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -2,12 +2,13 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @Author meditat0r
  * @create 2022/1/3 19:19
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
--- cloud-eureka-server-7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
+++ cloud-eureka-server-7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -2,13 +2,14 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @Author meditat0r
  * @create 2022/1/4 10:04
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableEurekaServer
 public class EurekaMain7001 {
     public static void main(String[] args) {
