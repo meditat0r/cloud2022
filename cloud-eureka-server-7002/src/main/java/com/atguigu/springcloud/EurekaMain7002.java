5b5ac205fb0fdff38189cdce904b2748e9ce981a
meditat0r <2523398873@qq.com>
2022-01-04 11:42:34 +0800

    Eureka集群环境搭建

---
+++ cloud-eureka-server-7002/src/main/java/com/atguigu/springcloud/EurekaMain7002.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author meditat0r
+ * @create 2022/1/4 11:30
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
