e0840a40df63dd7e8e56cadb3fac0decbfd8628e
meditat0r <2523398873@qq.com>
2022-01-05 23:28:56 +0800

    支付与订单模块注册到consul

---
+++ cloud-consumerconsul-order-80/src/main/java/com/atguigu/springcloud/OrderConsulMain80.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author meditat0r
+ * @create 2022/1/5 23:16
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+public class OrderConsulMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderConsulMain80.class, args);
+    }
+}
---
+++ cloud-providerconsul-payment-8006/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,24 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @Author meditat0r
+ * @create 2022/1/5 23:11
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "payment/consul")
+    public String paymentZk() {
+        return "SpringCloud with consul:" + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+}
