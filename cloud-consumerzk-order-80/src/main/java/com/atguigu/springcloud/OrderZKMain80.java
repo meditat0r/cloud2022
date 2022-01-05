02902201d7f825fe03b7305cb28669abd74f4526
meditat0r <2523398873@qq.com>
2022-01-05 16:10:43 +0800

    订单模块注册到zookeeper

---
+++ cloud-consumerzk-order-80/src/main/java/com/atguigu/springcloud/OrderZKMain80.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author meditat0r
+ * @create 2022/1/5 15:59
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+public class OrderZKMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderZKMain80.class, args);
+    }
+}
